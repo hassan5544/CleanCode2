44d2a5c6e180a40921808e95cb5a2c9ed7f267af
hassan5544 <hmh.lol55@gmail.com>
2023-10-20 14:50:08 +0300

    commit by hassan Add Unit Test to the Project 2023-10-20

---
+++ UnitTest/CalculationTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CleanCode2;
+
+namespace UnitTest
+{
+    public class CalculationTest
+    {
+        [Theory]
+        [InlineData(5, 25)]
+        [InlineData(2, 4)]
+        [InlineData(0,0)]
+        public void CalculateSquareTest(int number, int Expected)
+        {
+            //Act
+            int acutal = Calculation.CalculateSquare(number);
+
+            //Assert
+            Assert.Equal(Expected, acutal);
+        }
+    }
+}
---
+++ UnitTest/UserInteractionTest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CleanCode2;
+namespace UnitTest
+{
+    public class UserInteractionTest
+    {
+
+    }
+}
